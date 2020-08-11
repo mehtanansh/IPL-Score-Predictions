818e4b744855990b05a37fd9d6b8b77dd04c6f95
Ansh Mehta <ansh.mehta@somaiya.edu>
2020-08-11 23:41:29 +0530

    Fifth Commit

--- IPL_Predictions/__init__.py
+++ IPL_Predictions/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 import os
 app = Flask(__name__)
 
-app.config['SECRET_KEY']=os.environ['SK']
+app.config['SECRET_KEY']="271faea3aee44b3d1011bd01be4dca553a654a8fae852641"
 
 
 from IPL_Predictions import routes
